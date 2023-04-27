abba23da8b9c82e702e449c036cd5a5fe7257378
LopeKinz <48872005+LopeKinz@users.noreply.github.com>
2023-04-27 12:16:31 +0000

    safdjk

---
+++ main.py
@@ -0,0 +1,74 @@
+import os
+import random
+
+def encrypt(key, filename):
+    chunk_size = 64 * 1024
+    output_file = filename + ".crypt"
+    filesize = str(os.path.getsize(filename)).zfill(16)
+    IV = ''
+
+    for i in range(16):
+        IV += chr(random.randint(0, 0xFF))
+
+    with open(filename, 'rb') as input_file:
+        with open(output_file, 'wb') as output_file:
+            output_file.write(filesize.encode())
+            output_file.write(IV.encode())
+
+            while True:
+                chunk = input_file.read(chunk_size)
+
+                if len(chunk) == 0:
+                    break
+
+                elif len(chunk) % 16 != 0:
+                    chunk += b' ' * (16 - len(chunk) % 16)
+
+                xor_chunk = ''
+                for i in range(16):
+                    xor_chunk += chr(ord(chunk[i]) ^ ord(key[i]))
+
+                output_file.write(xor_chunk.encode())
+
+def decrypt(key, filename):
+    chunk_size = 64 * 1024
+    output_file = filename[:-6]
+    filesize = int(filename[-22:-6])
+    IV = filename[-6:]
+
+    with open(filename, 'rb') as input_file:
+        with open(output_file, 'wb') as output_file:
+            while True:
+                chunk = input_file.read(chunk_size)
+
+                if len(chunk) == 0:
+                    break
+
+                xor_chunk = ''
+                for i in range(16):
+                    xor_chunk += chr(ord(chunk[i]) ^ ord(key[i]))
+
+                output_file.write(xor_chunk.encode())
+
+            output_file.truncate(filesize)
+
+def main():
+    choice = input("Encrypt (E) or Decrypt (D)? ")
+
+    if choice == 'E':
+        filename = input("File to encrypt: ")
+        key = input("Encryption key: ")
+        encrypt(key, filename)
+        print("File encrypted.")
+
+    elif choice == 'D':
+        filename = input("File to decrypt: ")
+        key = input("Decryption key: ")
+        decrypt(key, filename)
+        print("File decrypted.")
+
+    else:
+        print("Invalid choice.")
+
+if __name__ == '__main__':
+    main()
